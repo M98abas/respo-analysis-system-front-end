e20de41f776f1a89e47e1e98383a0e7f93457de9
m98abas <mahmoud.ud@gmail.com>
2022-11-22 20:49:00 +0300

    this commit I finished add table to take the data and show it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,109 @@
+import { Line } from "@ant-design/charts";
 import { InboxOutlined } from "@ant-design/icons";
-import {  Upload } from "antd";
-import { useState } from "react";
+import { Upload, Table, message } from "antd";
+import moment from "moment";
+import { useEffect, useState } from "react";
 
 const { Dragger } = Upload;
 
 export default function Home() {
+  const [dataSource, setDataSource]: any = useState([]);
+  const [data, setData]: any = useState([]);
+  const [firstBin, setFirstBin]: any = useState("");
+  const [secondBin, setSecondBin]: any = useState("");
   const [image, setImage]: any = useState();
-  let state:any = {
+  const bins: any = [firstBin, secondBin];
+  const dateTime = moment(new Date()).format("YYYY/MM/DD");
+  let state: any = {
     loading: false, // to keep track of when form submitted
     errors: null, // for displaying errors
-    file: '', // the file type the user chooses to download
-  }
+    file: "", // the file type the user chooses to download
+  };
+  let config: any;
+
+  const columns = [
+    {
+      title: "Bin",
+      dataIndex: "Bin",
+      key: "Bin",
+    },
+    {
+      title: "response_code",
+      dataIndex: "response_code",
+      key: "response_code",
+    },
+
+    {
+      title: "response_description",
+      dataIndex: "response_description",
+      key: "response_description",
+    },
+    {
+      title: "Count",
+      dataIndex: "count",
+      key: "Count",
+    },
+    {
+      title: "children",
+      dataIndex: "children",
+      children: (data: any) => [
+        {
+          title: "Bin",
+
+          dataIndex: "children.Bin",
+        },
+      ],
+      // key:"children",
+    },
+  ];
+  //   useEffect(()=>{
+  // sendData
+  //   },[data])
   function sendData() {
+    if (image) {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var formdata = new FormData();
+      formdata.append("file", image, image.name);
+      var requestOptions: any = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+      };
+
+      fetch(`http://127.0.0.1:8000/uplaod`, requestOptions)
+        .then((response) => response.json())
+        .then(async (blob) => {
+          setDataSource(blob);
+        })
+        .catch((e) => console.log(e));
+    } else {
+      return message.error("Image is not set");
+    }
+  }
+
+  function analyizedDataUploadButton() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     var formdata = new FormData();
     formdata.append("file", image, image.name);
+    // formdata.append("bin_find", bins);
     var requestOptions: any = {
       method: "POST",
       body: formdata,
       redirect: "follow",
     };
 
-    fetch(`https://glacial-waters-71430.herokuapp.com/uplaod`, requestOptions)
-      .then((response) => response.blob())
-      .then(async (blob) => {
-        console.log(blob);
-        // 2. Create blob link to download
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link: any = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", `sample.${state.file}.csv`); // 3. Append to html page
-        document.body.appendChild(link); // 4. Force download
-        link.click(); // 5. Clean up and remove the link
-        link.parentNode.removeChild(link);
-        // router
+    fetch(`http://127.0.0.1:8000/analize`, requestOptions)
+      .then((response) => response.json())
+      .then(async (result: any) => {
+        setDataSource(result);
       })
       .catch((e) => console.log(e));
+
+    // return <Line {...config} />;
   }
 
   const handleImageUpload = ({ fileList }: any) => {
@@ -45,7 +111,18 @@ export default function Home() {
       setImage(fileList[0].originFileObj);
     }
   };
-  
+
+  config = {
+    data,
+    height: 400,
+    xField: "Code",
+    yField: "Date",
+    point: {
+      size: 5,
+      shape: "diamond",
+    },
+  };
+
   return (
     <>
       <div className="main-container">
@@ -63,16 +140,28 @@ export default function Home() {
             </p>
           </Dragger>
         </div>
-        <button className="btn" onClick={sendData}>Upload</button>
+        <div className="flexed-items">
+          <button className="btn" onClick={sendData}>
+            Get Codes
+          </button>
+          {/* <button className="btn" onClick={analyizedDataUploadButton}>
+            Get analize
+          </button> */}
+        </div>
+        {/* <div className="flexed-items input-items">
+          <div className="items">
+            <p>First Bin</p>
+            <input type="text" onChange={(e) => setFirstBin(e.target.value)} />
+          </div>
+          <div className="items">
+            <p>Second Bin</p>
+            <input type="text" onChange={(e) => setSecondBin(e.target.value)} />
+          </div>
+        </div> */}
       </div>
-      <div className="chart-section">
-        <div className="container">
-          <div className="right-sec">
-
-          </div>
-          <div className="left-sec">
-            
-          </div>
+      <div className="table">
+        <div className="table-container">
+          <Table dataSource={dataSource} columns={columns} bordered />
         </div>
       </div>
     </>
